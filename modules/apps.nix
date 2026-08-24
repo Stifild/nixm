4dd1209579696e21b7b7526b6be29f5e2aa11d9c
Kirill <dev@stifild.ru>
2026-08-24 21:15:18 +0300

    asdas

--- modules/apps.nix
+++ modules/apps.nix
@@ -1,26 +1,28 @@
 { pkgs, ... }:
 {
-  services.flatpak = {
-    enable = true;
+  # services.flatpak = {
+  #   enable = true;
     
-    # Добавляем Orion Beta репозиторий
-    remotes = [
-          {
-      name = "flathub";
-      location = "https://dl.flathub.org/repo/flathub.flatpakrepo";
-    }
-    {
-      name = "orion-beta";
-      location = "https://flatpak.orionbrowser.com/orion-beta.flatpakrepo";
-    }
-    ];
-    packages = [
-      "org.gnome.Platform/x86_64/50"
-      { appId = "com.kagi.Orion"; origin = "orion-beta"; }
-    ];
-  };
+  #   # Добавляем Orion Beta репозиторий
+  #   remotes = [
+  #         {
+  #     name = "flathub";
+  #     location = "https://dl.flathub.org/repo/flathub.flatpakrepo";
+  #   }
+  #   {
+  #     name = "orion-beta";
+  #     location = "https://flatpak.orionbrowser.com/orion-beta.flatpakrepo";
+  #   }
+  #   ];
+  #   packages = [
+  #     "org.gnome.Platform/x86_64/50"
+  #     { appId = "com.kagi.Orion"; origin = "orion-beta"; }
+  #   ];
+  # };
 
   xdg.portal.enable = true;
+
+  programs.firefox.enable = true;
 
   environment.systemPackages = with pkgs; [
     onlyoffice-desktopeditors
