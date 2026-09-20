011d91a0cd5e590f8f2862270b122c3bb233358e
Kirill <dev@stifild.ru>
2026-09-20 14:12:44 +0300

    Enable NVIDIA graphics support in configuration.nix

--- hosts/mswax-pc/configuration.nix
+++ hosts/mswax-pc/configuration.nix
@@ -5,6 +5,9 @@
   ];
 
   networking.hostName = "mswax-pc";
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.nvidia.open = true;
+  hardware.graphics.enable = true;
 
   system.stateVersion = "26.05"; #DONT CHANGE IT!
 }
