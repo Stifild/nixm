ff75a74005bd1ca516e93ee4a9d6e6a76e690c24
Kirill <dev@stifild.ru>
2026-09-20 18:25:23 +0300

    Disable silent boot settings in base.nix
    Comment out console log level, initrd verbosity, and kernel parameters for silent boot.

--- modules/base.nix
+++ modules/base.nix
@@ -8,13 +8,13 @@
     };
 
     # Enable "Silent boot"
-    consoleLogLevel = 3;
-    initrd.verbose = false;
-    kernelParams = [
-      "quiet"
-      "rd.udev.log_level=3"
-      "rd.systemd.show_status=auto"
-    ];
+    # consoleLogLevel = 3;
+    # initrd.verbose = false;
+    # kernelParams = [
+    #   "quiet"
+    #   "rd.udev.log_level=3"
+    #   "rd.systemd.show_status=auto"
+    # ];
 
     # Hide the OS choice for bootloaders.
     # It's still possible to open the bootloader list by pressing any key
