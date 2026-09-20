cdedc0201853e4d6316897332e4581f2017bd85c
Kirill <dev@stifild.ru>
2026-09-20 23:33:15 +0300

    Add 'veth-herm0' to trusted interfaces for firewall

--- modules/networking.nix
+++ modules/networking.nix
@@ -3,7 +3,7 @@
   networking.networkmanager.enable = true;
 
   services.tailscale.enable = true;
-  networking.firewall.trustedInterfaces = [ "tailscale0" ];
+  networking.firewall.trustedInterfaces = [ "tailscale0", "veth-herm0" ];
   networking.firewall.allowedUDPPorts = [ config.services.tailscale.port ];
   networking.firewall.allowedTCPPorts = [ 9119 ];
 }
