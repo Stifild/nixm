97eecdc128d5502d084638f48ac98de3d777afd3
Kirill <dev@stifild.ru>
2026-09-20 12:38:28 +0300

    Add TCP port 9119 to allowed firewall ports

--- modules/networking.nix
+++ modules/networking.nix
@@ -5,4 +5,5 @@
   services.tailscale.enable = true;
   networking.firewall.trustedInterfaces = [ "tailscale0" ];
   networking.firewall.allowedUDPPorts = [ config.services.tailscale.port ];
+  networking.firewall.allowedTCPPorts = [ 9119 ];
 }
